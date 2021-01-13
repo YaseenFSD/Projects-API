dc5ae31772a8e06bf6d99c6608a074b54d6cbb15
Yaseen Al-Salamy <yhalsalamy@gmail.com>
2021-01-13 12:59:13 -0500

    Refactored checkKey middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,33 +3,33 @@ import { ObjectId } from "mongodb"
 import { db } from "./db"
 
 export const checkKey = async (req: Request, res: Response, next: NextFunction) => {
+    if (!req.query.api_key) {
+        res.status(401).send({
+            "StatusCode": 401,
+            "Message": "Missing API key"
+        })
+        return
+    }
+    
     let key
+
     try {
         key = await db.collection("api_keys").findOne(new ObjectId(req.query.api_key as string))
-        if (key === null) {
-            throw new Error("No Key was found")
-        }
     } catch (error) {
-        if (req.query.api_key === undefined || req.query.api_key === "") {
-            res.status(401).send({
-                "StatusCode": 401,
-                "Message": "Missing API key"
-            })
-        } else {
             res.status(401).send({
                 "StatusCode": 401,
                 "Message": "Incorrect API key"
             })
-        }
+
         return
     }
-    if (!key.enabled) {
-        res.status(403).send({
-            "StatusCode": 403,
-            "Message": "Key disabled"
-        })
-    } else {
-        next()
-    }
 
+        if (!key.enabled) {
+            res.status(403).send({
+                "StatusCode": 403,
+                "Message": "Key disabled"
+            })
+        } else {
+            next()
+        }
 }
